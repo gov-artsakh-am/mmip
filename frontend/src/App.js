569b18ec278e1d7202742719d56dcbc5f9d2bb73
Marat Hakobjanyan <marat.hakobjanyan9@gmail.com>
2021-07-03 21:29:38 +0400

    feat: setup react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router";
 
-const App = () => <div>MMIS</div>;
+import "./App.css";
+import Auth from "./pages/Auth/Auth";
+
+const App = () => {
+  const routes = (
+    <Switch>
+      <Route path="/" component={Auth} />
+    </Switch>
+  );
+
+  return <div>{routes}</div>;
+};
 
 export default App;
---
+++ frontend/src/pages/Auth/Auth.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Auth.css";
+
+const Auth = () => {
+  return <div className="Auth">Auth screen</div>;
+};
+
+export default Auth;
