320d9e5fed4e54035be6534963c832ca79ff9ee9
Tigran Balayan <tigran_b@instigatemobile.com>
2021-07-03 21:35:42 +0400

    chore(frontend): Setup redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store'
+
 import App from './App';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('app'),
 );
