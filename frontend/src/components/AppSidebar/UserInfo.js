06cec3ebf95cfea9722dd4640fe219f6e8ea5655
Tigran Balayan <tigran_b@instigatemobile.com>
2021-07-05 06:30:06 +0400

    Add logout and name functionality

--- frontend/src/components/AppSidebar/UserInfo.js
+++ frontend/src/components/AppSidebar/UserInfo.js
@@ -1,39 +1,66 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+
 import userAvatar from '../../assets/vectors/userAvatar.svg';
 import userSettingIcon from '../../assets/vectors/userSettings.svg';
 import settingIcon from '../../assets/vectors/settings.svg';
 import logoutIcon from '../../assets/vectors/logout.svg';
+import { decodeToken } from '../../utils/jwt';
 
-const MENU_ITEMS = [{ icon: userSettingIcon, label: 'Օգտատիրոջ կարգավորումներ' }, { icon: settingIcon, label: 'Կարգավորումներ' }, { icon: logoutIcon, label: 'Ելք' }];
+const MENU_ITEMS = [{
+  icon: userSettingIcon,
+  label: 'Օգտատիրոջ կարգավորումներ'
+}, {
+  icon: settingIcon,
+  label: 'Կարգավորումներ'
+}, {
+  icon: logoutIcon,
+  label: 'Ելք',
+  action: 'logout'
+}];
 
-const UserInfo = ({ firstName, lastName }) => (
-  <div className="userInfoContainer">
-    <div
-      selected={false}
-      onClick={() => {
-        // TODO: Do necessary action
-      }}
-      className="withBorder sidebarMenuItemContainer"
-    >
-      <img src={userAvatar} alt="userAvatar" />
-      <div className="sidebarMenuItemText">
-        {firstName} {lastName}
-      </div>
-    </div>
-    {MENU_ITEMS.map(({ icon, label }) => (
+function UserInfo() {
+  const history = useHistory();
+  const token = localStorage.getItem('token');
+  const user = decodeToken(token);
+  const actions = {
+    logout: () => {
+      localStorage.removeItem('token');
+      history.length = 1;
+      history.replace('/');
+    }
+  };
+  return (
+    <div className="userInfoContainer">
       <div
-        key={label}
         selected={false}
         onClick={() => {
           // TODO: Do necessary action
         }}
-        className="sidebarMenuItemContainer"
+        className="withBorder sidebarMenuItemContainer"
       >
-        <img src={icon} alt={label} />
-        <div className="sidebarMenuItemText">{label}</div>
+        <img src={userAvatar} alt="userAvatar" width="40px" height="40px" />
+        <div className="sidebarMenuItemText">
+          {user.name} {user.surname}
+        </div>
       </div>
-    ))}
-  </div>
-);
+      {MENU_ITEMS.map(({ icon, label, action }) => (
+        <div
+          key={label}
+          selected={false}
+          onClick={() => {
+            if (actions[action]) {
+              actions[action]();
+            }
+          }}
+          className="sidebarMenuItemContainer"
+        >
+          <img src={icon} alt={label} />
+          <div className="sidebarMenuItemText">{label}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default UserInfo;
